173748b0a26fd0796be0cd7dbe336bd9b5d13031
anandhu <anandhuuthaman6@gmail.com>
2024-05-20 20:28:50 +0530

    first commit

--- calc_app/views.py
+++ calc_app/views.py
@@ -19,24 +19,3 @@ def calculate(request):
             return render(request, 'index.html', {'result': result})
 
     return render(request, 'index.html', {'error_message': "Method not allowed"})
-
-    if request.method == 'POST':
-        num1 = float(request.POST['num1'])
-        num2 = float(request.POST['num2'])
-        operation = request.POST['operation']
-    
-        if operation == 'add':
-            result = num1 + num2
-        elif operation == 'subtract':
-            result = num1 - num2
-        elif operation == 'multiply':
-            result = num1 * num2
-        elif operation == 'divide':
-            if num2 != 0:
-                result = num1 / num2
-            else:
-                result = "Error: Cannot divide by zero"
-        else:
-            result = "Invalid operation"
-            
-        return render(request, 'index.html', {'result': result})
